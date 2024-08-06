c5f47b8e4e77334a855574c5c546f8bbdfa57cef
RomanEvgenovichLoboda <loboda.rom@gmail.com>
2024-08-06 14:27:23 +0300

    first commit

---
+++ Scripts/script.js
@@ -0,0 +1,34 @@
+
+document.addEventListener('DOMContentLoaded', function (e) {
+    // document.querySelectorAll(".dropdown-toggle").forEach(e=>{
+    //     e.addEventListener("click", e=>{
+    //         const menu = e.currentTarget.dataset.path;
+    //         console.log('menu - '+menu);
+    //         const dropdown = document.querySelector('#'+menu);
+    //         // document.querySelectorAll(".dropdown-menu").forEach(e=>{
+    //             // const menuItem = e.currentTarget;
+    //             // console.log('menuItem - '+ menuItem);
+    //             // if(!document.querySelector('[data-target=${menu}]').classList.contains('open')){
+    //             //     document.querySelector('[data-target=${menu}]').classList.add('menu-active');
+    //             //
+    //             // }
+    //         // })
+    //     });
+    // });
+    const menuButtons = document.querySelectorAll('.dropdown-toggle');
+    menuButtons.forEach(menuButton => {
+        menuButton.addEventListener('click', (e) => {
+            const path = menuButton.dataset.path;
+            console.log('path - '+path);
+            const menu = document.getElementById(path);
+            if(menu.classList.contains('menu-hidden')===true) {
+                menu.classList.remove('menu-hidden');
+                menu.classList.add('menu-active');
+            }
+            else{
+                menu.classList.remove('menu-active');
+                menu.classList.add('menu-hidden');
+            }
+        })
+    })
+});
